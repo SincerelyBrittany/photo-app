5c0d7680e0146650217cedaa73a8ab0b4f1e0489
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-21 11:27:06 -0400

    uses jquery to create an initial function that finds the add-photo class and adds the click event to push the photos into an array, then it uses the submit id to send a post request to /photos create method with the photos object

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,48 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+
+
+// (function() {
+//     $( ".add-photo" ).click(function() {
+//         alert( "Handler for .click() called." );
+//       });
+//     // function addPhoto(e){
+//     //     console.log(e.target.dataset.url)
+//     //     console.log(e.target.dataset.description)
+//     // }
+//     // const photos = document.getElementByClass(".add-photo");
+//     // photos.addEventListener('click', addPhoto, false)
+// })()
+
+$(document).on("turbolinks:load", function() {
+    const photos = []
+    $( ".add-photo" ).click(function(e) {
+        // console.log(e.target.dataset.url)
+        // console.log(e.target.dataset.description)
+        const photo = {url: e.target.dataset.url, quote: e.target.dataset.description }
+        photos.push(photo)
+        console.log(photos)
+    });
+    $("#submit").click(function(e){
+    fetch('/photos', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({data: photos}),
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log('Success:', data);
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
+    })
+})
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
